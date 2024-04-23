fdb5da324224f89fef7ee22becd95779b87e59b3
AngelGonzs <angel.gonzalez49@upr.edu>
2024-04-23 17:17:36 -0400

    Deleted commented out code

--- Task-4/sprites.asm
+++ Task-4/sprites.asm
@@ -348,37 +348,6 @@ forever:
   RTS 
 .endproc
 
-; LoadBackground:
-;   LDA $2002             ; read PPU status to reset the high/low latch
-;   LDA #$20
-;   STA $2006             ; write the high byte of $2000 address
-;   LDA #$00
-;   STA $2006             ; write the low byte of $2000 address
-;   LDX #$00              ; start out at 0
-
-; LoadBackgroundLoop:
-;   LDA background, x     ; load data from address (background + the value in x)
-;   STA $2007             ; write to PPU
-;   INX                   ; X = X + 1
-;   CPX #$80              ; Compare X to hex $80, decimal 128 - copying 128 bytes
-;   BNE LoadBackgroundLoop  ; Branch to LoadBackgroundLoop if compare was Not Equal to zero
-;                         ; if compare was equal to 128, keep going down
-
-; LoadAttribute:
-;   LDA $2002             ; read PPU status to reset the high/low latch
-;   LDA #$23
-;   STA $2006             ; write the high byte of $23C0 address
-;   LDA #$C0
-;   STA $2006             ; write the low byte of $23C0 address
-;   LDX #$00              ; start out at 0
-
-; LoadAttributeLoop:
-;   LDA attribute, x      ; load data from address (attribute + the value in x)
-;   STA $2007             ; write to PPU
-;   INX                   ; X = X + 1
-;   CPX #$08              ; Compare X to hex $08, decimal 8 - copying 8 bytes
-;   BNE LoadAttributeLoop
-
 .segment "VECTORS"
 .addr nmi_handler, reset_handler, irq_handler
 
@@ -413,12 +382,6 @@ stage1left:
 
 masks:
   .byte %11000000, %00110000, %00001100, %00000011
-
-; sprites:
-;   .byte $70, $04, $00, $80
-;   .byte $70, $05, $00, $88
-;   .byte $78, $14, $00, $80
-;   .byte $78, $15, $00, $88
 
 .segment "CHR"
 .incbin "graphics.chr"
