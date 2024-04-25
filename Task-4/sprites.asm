2b9d324e422f33e8406fd4bcdaa745deb91ac361
nelson-escalona <nelson.escalona@upr.edu>
2024-04-25 13:41:04 -0400

    Backgrounds are now working and attribute bytes are written.

--- Task-4/sprites.asm
+++ Task-4/sprites.asm
@@ -41,23 +41,27 @@ ppuctrl_settings: .res 1
 
   ; update tiles *after* DMA transfer
 
-  LDA scroll
-  CMP #$00
-  BNE set_scroll_positions
+;   LDA scroll
+;   CMP #$00
+;   BNE set_scroll_positions
 
-  LDA ppuctrl_settings
-  EOR #%00000010
-  STA ppuctrl_settings
-  STA PPUCTRL
-  LDA #240
-  STA scroll
+;   LDA ppuctrl_settings
+;   EOR #%00000010
+;   STA ppuctrl_settings
+;   STA PPUCTRL
+;   LDA #240
+;   STA scroll
 
-set_scroll_positions:
-  DEC scroll
-  LDA scroll
-  STA PPUSCROLL
+; set_scroll_positions:
+;   DEC scroll
+;   LDA scroll
+;   STA PPUSCROLL
+;   LDA #$00
+;   STA PPUSCROLL
+
   LDA #$00
-  STA PPUSCROLL
+  STA $2005
+  STA $2005
 
   RTI
 .endproc
@@ -66,8 +70,8 @@ set_scroll_positions:
 
 .export main
 .proc main
-  LDA #239
-  STA scroll
+  ; LDA #239
+  ; STA scroll
   ; write a palette
   LDX PPUSTATUS
   LDX #$3f
@@ -77,19 +81,22 @@ set_scroll_positions:
 load_palettes:
   LDA palettes,X
   STA PPUDATA
-  INX
+  INX                                                                                                                                 
   CPX #$20
   BNE load_palettes
+
+  JSR load_M_segment
+  JSR load_M_segment2
 
 vblankwait:       ; wait for another vblank before continuing
   BIT PPUSTATUS
   BPL vblankwait
 
-  JSR load_M_segment
-  JSR load_M_segment2
+  ;JSR load_M_segment
+  ;JSR load_M_segment2
 
   LDA #%10010000  ; turn on NMIs, sprites use first pattern table
-  STA ppuctrl_settings
+  ; STA ppuctrl_settings
   STA PPUCTRL
   LDA #%10011110  ; turn on screen
   STA PPUMASK
@@ -115,6 +122,7 @@ forever:
     ;       MXb = Mindex%4 (or Mindex&&0x03)
 
     LDA m_index ; load the current M index into A
+    CLC
     LSR A       ; first shift right
     LSR A       ; second shift right
     STA MYb     ; store A into MYb
@@ -139,6 +147,7 @@ forever:
     
     ; 1. Shift MYb twice and Mask it
     LDA MYb
+    CLC
     LSR A
     LSR A
     AND #$03
@@ -146,6 +155,7 @@ forever:
 
     ; 2.1 Shift MXb 3 times left.
     LDA MXb
+    CLC
     ASL A
     ASL A
     ASL A
@@ -153,6 +163,7 @@ forever:
 
     ; 2.2 Shift MYb  6 times left.
     LDA MYb
+    CLC
     ASL A
     ASL A
     ASL A
@@ -161,6 +172,7 @@ forever:
     ASL A
 
     ; 2.3 Add the two and store it.
+    CLC
     ADC MXb
     STA index_low
 
@@ -176,12 +188,13 @@ forever:
 
     ; Increase m_index
     LDX m_index
+    CLC
     INX 
     STX m_index
 
     ; Check if Mindex != 59 (last index)
     LDX m_index
-    CPX #$3B
+    CPX #$3C
     BNE LoopMindex
     LDA #$00
     STA m_index
@@ -234,10 +247,10 @@ forever:
   ; PREP: Before getting into the Loop Below, increase index_low
   ;       by +6 so that it starts at the last Mega Tile and we will
   ;       
-  LDA index_low
-  CLC
-  ADC #$06
-  STA index_low
+  ; LDA index_low
+  ; CLC
+  ; ADC #%00000110
+  ; STA index_low
 
   ; 1.1 Get byte from nametable and store it.
   ; NOTE : Might have to load m_index to Y so that
@@ -274,6 +287,12 @@ forever:
     ; NOTE : We start at the 4th mega tile. CASE: MINDEX = 48
     LDA current_byte  ; = 01111111
     AND #%00000011    ; = 00000011
+    ; LSR A
+    ; LSR A
+    ; LSR A
+    ; LSR A
+    ; LSR A
+    ; LSR A
     STA current_mega  ; = 00000011
 
     ;;;;;;;;;;; GOOD UP TIL NOW
@@ -308,6 +327,7 @@ forever:
     ADC #$20
     STA PPUADDR     ; Write the high byte
     LDY index_low   ; Increase index_low
+    CLC
     INY             ; and store in low bit
     STY PPUADDR
     ; Write Data to INDEX+1  |  Offset = 1
@@ -324,7 +344,7 @@ forever:
     STA PPUADDR   ; Write the high byte
     LDA index_low
     CLC
-    ADC #$20      ; Add 32! 
+    ADC #32      ; Add 32! 
     STA PPUADDR   ; store it in low bit
     ; Write Data to INDEX+32  |  Offset = 2
     LDA dec_mega
@@ -340,7 +360,7 @@ forever:
     STA PPUADDR   ; Write the high byte
     LDA index_low
     CLC
-    ADC #$21      ; Add 33! 
+    ADC #33      ; Add 33! 
     STA PPUADDR   ; store it in low bit
     ; Write Data to INDEX+33  |  Offset = 3
     LDA dec_mega
@@ -356,16 +376,18 @@ forever:
     ; this is done so that we can move to the prev megatile
     LDA index_low
     CLC
-    SBC #$02
+    ADC #$02
     STA index_low
 
     ; Shift our curr_byte, as shown in the `MEGA_NAMETABLE` sheet.
     LDA current_byte
+    CLC
     LSR A
     LSR A
     STA current_byte
 
     ; Loop Condition, if X != 4, Loop
+    CLC
     INX                 ; X += 1
     CPX #$04            ; I forgot how the BEQ worke
     BNE Iter_megatile   ; if X != 4, keep loopin!
@@ -399,6 +421,7 @@ forever:
     ;       MXb = Mindex%4 (or Mindex&&0x03)
 
     LDA m_index ; load the current M index into A
+    CLC
     LSR A       ; first shift right
     LSR A       ; second shift right
     STA MYb     ; store A into MYb
@@ -423,6 +446,7 @@ forever:
     
     ; 1. Shift MYb twice and Mask it
     LDA MYb
+    CLC
     LSR A
     LSR A
     AND #$03
@@ -430,6 +454,7 @@ forever:
 
     ; 2.1 Shift MXb 3 times left.
     LDA MXb
+    CLC
     ASL A
     ASL A
     ASL A
@@ -437,6 +462,7 @@ forever:
 
     ; 2.2 Shift MYb  6 times left.
     LDA MYb
+    CLC
     ASL A
     ASL A
     ASL A
@@ -445,6 +471,7 @@ forever:
     ASL A
 
     ; 2.3 Add the two and store it.
+    CLC
     ADC MXb
     STA index_low
 
@@ -460,13 +487,16 @@ forever:
 
     ; Increase m_index
     LDX m_index
+    CLC
     INX 
     STX m_index
 
     ; Check if Mindex != 59 (last index)
     LDX m_index
-    CPX #$3B
-    BNE LoopMindex 
+    CPX #$3C
+    BNE LoopMindex
+    LDA #$00
+    STA m_index
 
   PLA 
   TYA
@@ -516,10 +546,10 @@ forever:
   ; PREP: Before getting into the Loop Below, increase index_low
   ;       by +6 so that it starts at the last Mega Tile and we will
   ;       
-  LDA index_low
-  CLC
-  ADC #$06
-  STA index_low
+  ; LDA index_low
+  ; CLC
+  ; ADC #%00000110
+  ; STA index_low
 
   ; 1.1 Get byte from nametable and store it.
   ; NOTE : Might have to load m_index to Y so that
@@ -556,6 +586,12 @@ forever:
     ; NOTE : We start at the 4th mega tile. CASE: MINDEX = 48
     LDA current_byte  ; = 01111111
     AND #%00000011    ; = 00000011
+    ; LSR A
+    ; LSR A
+    ; LSR A
+    ; LSR A
+    ; LSR A
+    ; LSR A
     STA current_mega  ; = 00000011
 
     ;;;;;;;;;;; GOOD UP TIL NOW
@@ -590,6 +626,7 @@ forever:
     ADC #$24
     STA PPUADDR     ; Write the high byte
     LDY index_low   ; Increase index_low
+    CLC
     INY             ; and store in low bit
     STY PPUADDR
     ; Write Data to INDEX+1  |  Offset = 1
@@ -606,7 +643,7 @@ forever:
     STA PPUADDR   ; Write the high byte
     LDA index_low
     CLC
-    ADC #$20      ; Add 32! 
+    ADC #32      ; Add 32! 
     STA PPUADDR   ; store it in low bit
     ; Write Data to INDEX+32  |  Offset = 2
     LDA dec_mega
@@ -622,7 +659,7 @@ forever:
     STA PPUADDR   ; Write the high byte
     LDA index_low
     CLC
-    ADC #$21      ; Add 33! 
+    ADC #33      ; Add 33! 
     STA PPUADDR   ; store it in low bit
     ; Write Data to INDEX+33  |  Offset = 3
     LDA dec_mega
@@ -638,16 +675,18 @@ forever:
     ; this is done so that we can move to the prev megatile
     LDA index_low
     CLC
-    SBC #$02
+    ADC #$02
     STA index_low
 
     ; Shift our curr_byte, as shown in the `MEGA_NAMETABLE` sheet.
     LDA current_byte
+    CLC
     LSR A
     LSR A
     STA current_byte
 
     ; Loop Condition, if X != 4, Loop
+    CLC
     INX                 ; X += 1
     CPX #$04            ; I forgot how the BEQ worke
     BNE Iter_megatile   ; if X != 4, keep loopin!
@@ -725,10 +764,10 @@ forever:
 
 .segment "RODATA"
 palettes:
-  .byte $0f, $00, $10, $30
-  .byte $0f, $05, $16, $37
-  .byte $0f, $0B, $1A, $29
-  .byte $0f, $11, $11, $11
+  .byte $0f, $00, $10, $30 ;stone
+  .byte $0f, $05, $16, $37 ;brick
+  .byte $0f, $0B, $1A, $29 ;bush
+  .byte $0f, $0f, $0f, $0f 
 
   .byte $0f ,$11, $21, $01
   .byte $0f, $19, $09, $29
@@ -736,46 +775,90 @@ palettes:
   .byte $0f, $19, $09, $29
 
 stage1left:
+  ; This is the flipped/inverted map
   .byte %01010101, %01010101, %01010101, %01010101
-  .byte %01111111, %11111100, %10000000, %00000000
-  .byte %01111011, %11111100, %10001010, %10101010
-  .byte %01111010, %10101000, %10001000, %00000010
-  .byte %01000000,	%00001000, %10111010,	%10100110
-  .byte %01101010,	%10001000, %11111100,	%00101110
-  .byte %01000000,	%00001010, %10101010,	%10101110
-  .byte %01001010,	%10101000, %11111111, %11111110
-  .byte %01000000,	%00001000, %10101010,	%10101110
-  .byte %01101010,	%10001000, %10000000,	%00111110
-  .byte %01111111,	%11111000, %10111010,	%10100010
-  .byte %01111010,	%10101000, %10111000,	%00000010
-  .byte %01111111,	%11111100, %10111010,	%10100010
-  .byte %00111111,	%11111100, %10111111,	%11110010
-  .byte %01010101,	%01010101, %01010101,	%01010101
+  .byte %11111101, %11111111, %00000010, %00000000
+  .byte %11101101, %00111111, %10100010, %10101010
+  .byte %10101101, %00101010, %00100010, %10000000
+  .byte %00000001, %00100000, %10101110, %10111010
+  .byte %10101001, %00100010, %00111111, %10111000
+  .byte %00000001, %10100000, %10101010, %10111010
+  .byte %10100001, %00101010, %11111111, %10111111
+  .byte %00000001, %00100000, %10101010, %10111010
+  .byte %10101001, %00100010, %00000010, %10111100
+  .byte %11111101, %00101111, %10101110, %10001010
+  .byte %10101101, %00101010, %00101110, %10000000
+  .byte %11111101, %00111111, %10101110, %10001010
+  .byte %11111100, %00111111, %11111110, %10001111
+  .byte %01010101, %01010101, %01010101, %01010101
+  ; This is the actual map
+  ; .byte %01111111, %11111100, %10000000, %00000000
+  ; .byte %01111011, %11111100, %10001010, %10101010
+  ; .byte %01111010, %10101000, %10001000, %00000010
+  ; .byte %01000000,	%00001000, %10111010,	%10100110
+  ; .byte %01101010,	%10001000, %11111100,	%00101110
+  ; .byte %01000000,	%00001010, %10101010,	%10101110
+  ; .byte %01001010,	%10101000, %11111111, %11111110
+  ; .byte %01000000,	%00001000, %10101010,	%10101110
+  ; .byte %01101010,	%10001000, %10000000,	%00111110
+  ; .byte %01111111,	%11111000, %10111010,	%10100010
+  ; .byte %01111010,	%10101000, %10111000,	%00000010
+  ; .byte %01111111,	%11111100, %10111010,	%10100010
+  ; .byte %00111111,	%11111100, %10111111,	%11110010
+  ; .byte %01010101,	%01010101, %01010101,	%01010101
 
 stage1right:
+  ; This is the flipped/inverted map
   .byte %01010101, %01010101, %01010101, %01010101
-  .byte %00000010, %11111110, %00101010, %10101001
-  .byte %00100010, %11111110, %00000000, %00000001
-  .byte %00100010, %10100010, %10001010, %10100001
-  .byte %00100000, %00000010, %11111000, %00000001
-  .byte %00101010, %10101010, %11111010, %10101001
-  .byte %00100010, %00000010, %11111111, %11111101
-  .byte %00100010, %00100010, %10111010, %10111100
-  .byte %00100010, %00100011, %11111111, %10111101
-  .byte %00100010, %00101010, %10101010, %10111101
-  .byte %00101110, %11100000, %00000000, %10111101
-  .byte %00101111, %11110010, %10101000, %10111101
-  .byte %00101010, %10100010, %11111111, %10101001
-  .byte %00000000, %00000010, %11111011, %11111101
+  .byte %10000000, %10111111, %10101000, %01101010
+  .byte %10001000, %10111111, %00000000, %01000000
+  .byte %10001000, %10001010, %10100010, %01001010
+  .byte %00001000, %10000000, %00101111, %01000000
+  .byte %10101000, %10101010, %10101111, %01101010
+  .byte %10001000, %10000000, %11111111, %01111111
+  .byte %10001000, %10001000, %10101110, %00111110
+  .byte %10001000, %11001000, %11111111, %01111110
+  .byte %10001000, %10101000, %10101010, %01111110
+  .byte %10111000, %00001011, %00000000, %01111110
+  .byte %11111000, %10001111, %00101010, %01111110
+  .byte %10101000, %10001010, %11111111, %01101010
+  .byte %00000000, %10000000, %11101111, %01111111
   .byte %01010101, %01010101, %01010101, %01010101
+  ; This is the actual map
+  ; .byte %01010101, %01010101, %01010101, %01010101
+  ; .byte %00000010, %11111110, %00101010, %10101001
+  ; .byte %00100010, %11111110, %00000000, %00000001
+  ; .byte %00100010, %10100010, %10001010, %10100001
+  ; .byte %00100000, %00000010, %11111000, %00000001
+  ; .byte %00101010, %10101010, %11111010, %10101001
+  ; .byte %00100010, %00000010, %11111111, %11111101
+  ; .byte %00100010, %00100010, %10111010, %10111100
+  ; .byte %00100010, %00100011, %11111111, %10111101
+  ; .byte %00100010, %00101010, %10101010, %10111101
+  ; .byte %00101110, %11100000, %00000000, %10111101
+  ; .byte %00101111, %11110010, %10101000, %10111101
+  ; .byte %00101010, %10100010, %11111111, %10101001
+  ; .byte %00000000, %00000010, %11111011, %11111101
+  ; .byte %01010101, %01010101, %01010101, %01010101
 
 masks:
   .byte %11000000, %00110000, %00001100, %00000011
 
+attribute_stage1_left:
+  .byte %10000000, %10100000, %10100000, %11100000, %11010000, %11110000, %11110000, %11110000
+  .byte %10001000, %01011001, %01011010, %11011110, %11011101, %11010101, %11110101, %01110101
+  .byte %01001100, %01011111, %11011111, %11011101, %10101001, %11100101, %01110101, %01100110
+  .byte %11001100, %01011111, %01011111, %11010101, %10101001, %11100101, %01110101, %01100110
+  .byte %11001100, %01010000, %01010000, %00010101, %10100101, %10100101, %10100101, %01100110
+  .byte %01000000, %01010000, %00010000, %00010001, %00010101, %00000101, %10000101, %01100110
+  .byte %10001000, %01011010, %01011010, %00010001, %10011001, %00010101, %00000101, %01000100
+  .byte %10001000, %10101010, %10101010, %00100010, %10011001, %10100101, %10100101, %01000100
+
 .segment "CHR"
 .incbin "graphics.chr"
 
-
+; These are zeros to degub background map.
+; .byte %00000000, %00000000, %00000000, %00000000
 ; ca65 src/backgrounds.asm
 ; ca65 src/scrolling.asm
 ; ld65 src/backgrounds.o src/scrolling.o -C nes.cfg -o scrolling.nes
