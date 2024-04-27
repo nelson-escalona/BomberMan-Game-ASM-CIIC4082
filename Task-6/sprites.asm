250bf8da54e277dbd84e850ff736072adab72a1a
nelson-escalona <nelson.escalona@upr.edu>
2024-04-27 01:37:09 -0400

    Timer added and fixed task 4 glitching

--- Task-6/sprites.asm
+++ Task-6/sprites.asm
@@ -19,7 +19,26 @@ ppuctrl_settings: .res 1
 pad1: .res 1
 background_flag: .res 1
 checker: .res 1
-.exportzp m_index, pad1
+number0_x: .res 1
+number0_y: .res 1
+number1_x: .res 1
+number1_y: .res 1
+number2_x: .res 1
+number2_y: .res 1
+number0_tile: .res 1
+number1_tile: .res 1
+number2_tile: .res 1
+timer: .res 1
+timer1: .res 1
+timer2: .res 1
+timer_counter: .res 1
+timer_counter_1: .res 1
+timer_counter_2: .res 1
+player_x: .res 1
+player_y: .res 1
+sprite: .res 1
+
+.exportzp m_index, pad1, number0_x, number0_y, number0_tile, number1_x, number1_tile, number2_x, number2_tile, player_x, player_y, 
 
 .segment "CODE"
 .proc irq_handler
@@ -38,25 +57,36 @@ checker: .res 1
 
   JSR read_controller1
   JSR check_for_background_change
+  JSR draw_timer_0
+  JSR update_timer_0
+  JSR draw_timer_1
+  JSR update_timer_1
+  JSR draw_timer_2
+  JSR update_timer_2
 
   ; update tiles *after* DMA transfer
 
-  LDA scroll
-  CMP #$FF
-  BNE set_scroll_positions
+;   LDA scroll
+;   CMP #$00
+;   BNE set_scroll_positions
 
-  LDA ppuctrl_settings
-  EOR #%00000001
-  STA ppuctrl_settings
-  STA PPUCTRL
-  LDA #00
-  STA scroll
+;   LDA ppuctrl_settings
+;   EOR #%00000001
+;   STA ppuctrl_settings
+;   STA PPUCTRL
+;   LDA #00
+;   STA scroll
 
-set_scroll_positions:
-  INC scroll
-  LDA scroll
-  STA PPUSCROLL
+; set_scroll_positions:
+;   INC scroll
+;   LDA scroll
+;   STA PPUSCROLL
+;   LDA #$00
+;   STA PPUSCROLL
+
+  ; To stop scorlling
   LDA #$00
+  STA PPUSCROLL
   STA PPUSCROLL
 
   RTI
@@ -88,7 +118,7 @@ vblankwait:       ; wait for another vblank before continuing
   BIT PPUSTATUS
   BPL vblankwait
 
-  LDA #%10010001  ; turn on NMIs, sprites use first pattern table
+  LDA #%10010000  ; turn on NMIs, sprites use first pattern table
   STA ppuctrl_settings
   STA PPUCTRL
   LDA #%10011110  ; turn on screen
@@ -1334,7 +1364,7 @@ forever:
   JMP done_dec
 
   is_bg:         ; Black BG
-    LDA #$70
+    LDA #$FC
     STA dec_mega
     JMP done_dec
 
@@ -1388,7 +1418,7 @@ forever:
   JMP done_dec2
 
   is_bg2:         ; Black BG
-    LDA #$70
+    LDA #$FC
     STA dec_mega
     JMP done_dec2
 
@@ -1443,6 +1473,589 @@ forever:
   RTS 
 .endproc
 
+.proc draw_timer_0
+  PHP
+  PHA
+  TXA
+  PHA
+  TYA
+  PHA
+
+  ; Load tile number into OAM
+  LDA number0_tile
+  STA $0211
+  CLC
+  ADC #$01
+  STA $0215
+  CLC 
+  ADC #$01
+  STA $0219
+  CLC
+  ADC #$01
+  STA $021D
+
+  ; Load pallette into OAM
+  LDA #$01
+  STA $0212
+  STA $0216
+  STA $021A
+  STA $021E
+
+  ; Store tile locations
+  ; top left tile:
+  LDA number0_y
+  STA $0210
+  LDA number0_x
+  STA $0213
+
+  ; top right tile (x + 8):
+  LDA number0_y
+  STA $0214
+  LDA number0_x
+  CLC
+  ADC #$08
+  STA $0217
+
+  ; bottom left tile (y + 8):
+  LDA number0_y
+  CLC
+  ADC #$08
+  STA $0218
+  LDA number0_x
+  STA $021B
+
+  ; bottom right tile (x + 8, y + 8):
+  LDA number0_y
+  CLC
+  ADC #$08
+  STA $021C
+  LDA number0_x
+  CLC
+  ADC #$08
+  STA $021F
+
+  PLA 
+  TYA
+  PLA
+  TAX 
+  PLA 
+  PLP 
+  RTS 
+.endproc
+
+.proc update_timer_0
+  PHP
+  PHA
+  TXA
+  PHA
+  TYA
+  PHA
+
+  LDX timer
+  INX
+  CPX #$40
+  STX timer
+  BNE end_of_sub_0
+
+  LDX #$00
+  STX timer
+  LDA number0_tile
+  CLC
+  ADC #$04
+  STA number0_tile
+  LDX timer_counter
+  INX
+  CPX #$0A
+  STX timer_counter
+  BNE end_of_sub_0
+  LDX #$00
+  STX timer_counter
+  LDA #$54
+  STA number0_tile
+  LDA timer1
+  CLC
+  ADC #$01
+  STA timer1
+
+  end_of_sub_0:
+
+  PLA 
+  TYA
+  PLA
+  TAX 
+  PLA 
+  PLP 
+  RTS 
+.endproc
+
+.proc draw_timer_1
+  PHP
+  PHA
+  TXA
+  PHA
+  TYA
+  PHA
+
+  ; Load tile number into OAM
+  LDA number1_tile
+  STA $0221
+  CLC
+  ADC #$01
+  STA $0225
+  CLC 
+  ADC #$01
+  STA $0229
+  CLC
+  ADC #$01
+  STA $022D
+
+  ; Load pallette into OAM
+  LDA #$01
+  STA $0222
+  STA $0226
+  STA $022A
+  STA $022E
+
+  ; Store tile locations
+  ; top left tile:
+  LDA number1_y
+  STA $0220
+  LDA number1_x
+  STA $0223
+
+  ; top right tile (x + 8):
+  LDA number1_y
+  STA $0224
+  LDA number1_x
+  CLC
+  ADC #$08
+  STA $0227
+
+  ; bottom left tile (y + 8):
+  LDA number1_y
+  CLC
+  ADC #$08
+  STA $0228
+  LDA number1_x
+  STA $022B
+
+  ; bottom right tile (x + 8, y + 8):
+  LDA number1_y
+  CLC
+  ADC #$08
+  STA $022C
+  LDA number1_x
+  CLC
+  ADC #$08
+  STA $022F
+
+  PLA 
+  TYA
+  PLA
+  TAX 
+  PLA 
+  PLP 
+  RTS 
+.endproc
+
+.proc update_timer_1
+  PHP
+  PHA
+  TXA
+  PHA
+  TYA
+  PHA
+
+  LDA timer1
+  CMP #$01
+  BNE end_of_sub_1
+
+  LDA #$00
+  STA timer1
+  LDA number1_tile
+  CLC
+  ADC #$04
+  STA number1_tile
+  LDX timer_counter_1
+  INX
+  CPX #$0A
+  STX timer_counter_1
+  BNE end_of_sub_1
+  LDX #$00
+  STX timer_counter_1
+  LDA #$54
+  STA number1_tile
+  LDA timer2
+  CLC
+  ADC #$01
+  STA timer2
+
+  end_of_sub_1:
+
+  PLA 
+  TYA
+  PLA
+  TAX 
+  PLA 
+  PLP 
+  RTS 
+.endproc
+
+.proc draw_timer_2
+  PHP
+  PHA
+  TXA
+  PHA
+  TYA
+  PHA
+
+  ; Load tile number into OAM
+  LDA number2_tile
+  STA $0231
+  CLC
+  ADC #$01
+  STA $0235
+  CLC 
+  ADC #$01
+  STA $0239
+  CLC
+  ADC #$01
+  STA $023D
+
+  ; Load pallette into OAM
+  LDA #$01
+  STA $0232
+  STA $0236
+  STA $023A
+  STA $023E
+
+  ; Store tile locations
+  ; top left tile:
+  LDA number2_y
+  STA $0230
+  LDA number2_x
+  STA $0233
+
+  ; top right tile (x + 8):
+  LDA number2_y
+  STA $0234
+  LDA number2_x
+  CLC
+  ADC #$08
+  STA $0237
+
+  ; bottom left tile (y + 8):
+  LDA number2_y
+  CLC
+  ADC #$08
+  STA $0238
+  LDA number2_x
+  STA $023B
+
+  ; bottom right tile (x + 8, y + 8):
+  LDA number2_y
+  CLC
+  ADC #$08
+  STA $023C
+  LDA number2_x
+  CLC
+  ADC #$08
+  STA $023F
+
+  PLA 
+  TYA
+  PLA
+  TAX 
+  PLA 
+  PLP 
+  RTS 
+.endproc
+
+.proc update_timer_2
+  PHP
+  PHA
+  TXA
+  PHA
+  TYA
+  PHA
+
+  LDA timer2
+  CMP #$01
+  BNE end_of_sub_2
+
+  LDA #$00
+  STA timer2
+  LDA number2_tile
+  CLC
+  ADC #$04
+  STA number2_tile
+  LDX timer_counter_2
+  INX
+  CPX #$0A
+  STX timer_counter_2
+  BNE end_of_sub_2
+  LDX #$00
+  STX timer_counter_2
+  LDA #$54
+  STA number2_tile
+
+  end_of_sub_2:
+
+  PLA 
+  TYA
+  PLA
+  TAX 
+  PLA 
+  PLP 
+  RTS 
+.endproc
+
+.proc update_player
+  PHP  ; Start by saving registers,
+  PHA  ; as usual.
+  TXA
+  PHA
+  TYA
+  PHA
+
+  ; By default, we will assume that there is a button being pressed
+  ; if this is not the case, it will be corrected in `done_checking`
+  LDA #$01
+  STA animation
+
+
+  LDA pad1        ; Load button presses
+  AND #BTN_LEFT   ; Filter out all but Left
+  BEQ check_right ; If result is zero, left not pressed
+
+
+  DEC player_x  ; If the branch is not taken, move player left
+  LDX #$28        ; This is the first tile that looks left
+  STX sprite  ; Store it in sprite :)
+  JMP end_updt
+
+check_right:
+  LDA pad1
+  AND #BTN_RIGHT
+  BEQ check_up
+
+
+  INC player_x
+  LDX #$10        ; First Tile Looking Right       
+  STX sprite  ; Yup, we store it 
+  JMP end_updt
+
+check_up:
+  LDA pad1
+  AND #BTN_UP
+  BEQ check_down
+
+
+  DEC player_y
+  LDX #$04        ; First Tile Looking Up       
+  STX sprite  ; Yup, we store it here too
+  JMP end_updt
+
+check_down:
+  LDA pad1
+  AND #BTN_DOWN
+  BEQ done_checking
+
+
+  INC player_y
+  LDX #$1C        ; First Tile Looking Down       
+  STX sprite  ; Yup, last one.
+  JMP end_updt
+
+
+; This label indicates there was no button pressed, for which we will
+; just reset the offset and the tick! Also set `animation` to #$00
+done_checking:
+  LDA #$00
+  STA offset
+  STA tick
+  STA animation
+
+end_updt:
+  PLA ; Done with updates, restore registers
+  TAY ; and return to where we called this
+  PLA
+  TAX
+  PLA
+  PLP
+  RTS
+.endproc
+
+
+; The draw_player subroutine must also be update so that it's more
+; dynamic. We want it to render whatever is in `sprite` which could
+; change depending on the button being pressed.
+
+; Steps:
+;   1. Modify the subroutine so that it draws tiles based on the `sprite`.
+;
+;   2. Create, Update and Check the `Tick` so that we can animate.
+;
+;   3. Apply the same with the `offset`, so that we can animate the following
+;      tiles after `sprite` to form the animation.
+;
+;   4. Make sure to reset these when the animation is completed, I plan for this
+;   to take 20 ticks per sprite. Once we animate all ticks, return to original sprite
+;   which would be done by setting the offset to 0.
+;
+;   5. Set the conditional `animation` to decide whether we'll animate or not.
+
+.proc draw_player
+  ; save registers
+  PHP
+  PHA
+  TXA
+  PHA
+  TYA
+  PHA
+
+  ; LDA #$0C      ; Testing offset
+  ; STA offset    ; Seems to work fine!
+
+
+  ; Draw based on the `sprite` determined by button presses.
+  LDA sprite
+  CLC
+  ADC offset
+  STA $0201
+
+  LDA sprite
+  CLC
+  ADC offset
+  CLC
+  ADC #$01
+  STA $0205
+
+  LDA sprite
+  CLC
+  ADC offset
+  CLC
+  ADC #$02
+  STA $0209
+
+
+  LDA sprite
+  CLC
+  ADC offset
+  CLC
+  ADC #$03
+  STA $020d
+
+  ; write player ship tile attributes
+  ; use palette 0
+  LDA #$00
+  STA $0202
+  STA $0206
+  STA $020a
+  STA $020e
+
+  ; store tile locations
+  ; top left tile:
+  LDA player_y
+  STA $0200
+  LDA player_x
+  STA $0203
+
+  ; top right tile (x + 8):
+  LDA player_y
+  STA $0204
+  LDA player_x
+  CLC
+  ADC #$08
+  STA $0207
+
+  ; bottom left tile (y + 8):
+  LDA player_y
+  CLC
+  ADC #$08
+  STA $0208
+  LDA player_x
+  STA $020b
+
+  ; bottom right tile (x + 8, y + 8)
+  LDA player_y
+  CLC
+  ADC #$08
+  STA $020c
+  LDA player_x
+  CLC
+  ADC #$08
+  STA $020f
+
+
+  ; Use this section to check conditions.
+  ;   - Update Tick
+  ;   - Check Tick
+  ;
+  ;   - Update Tile/offset if necessary
+
+
+  ; Check if we're animating or not! If not, end draw_update
+  LDA animation
+  CMP #$00
+  BEQ end_draw
+
+  ; First, Increase the Tick
+  LDX tick
+  INX
+  STX tick
+
+
+  ; The Tick is in X, so check if it has hit either 20 or 40 ticks.
+  LDX tick
+  CPX #$14    ; This is 20 Decimal I think
+  BEQ move_sprite
+
+  CPX #$28
+  BEQ move_sprite
+
+
+  ; Now, we check if the sprite is greater than or equal to 60,
+  ; in which case we'd reset the sprite to it's first frame
+  CLC   ; Clear the Carry before the comparison we're about to make!
+  CPX #$3C    ; This is 60 Decimal
+  BEQ reset_sprite    ; Equal
+  BCS reset_sprite    ; Greater
+
+
+  ; If no comparison worked, go to end!
+  JMP end_draw
+
+  ; We have to move to the next sprite, so all we'd have to do is to 
+  ; move the offset + 4! (number of tiles per sprite = 4)
+  move_sprite:
+    LDA offset
+    CLC
+    ADC #$04
+    STA offset
+
+    JMP end_draw    ; JMP to End to avoid going into any other labels
+
+  ; In this case, we'd have to set the offset back to $00 and the tick as well!
+  reset_sprite:
+    LDA #$00
+    STA offset
+    STA tick
+
+
+
+  end_draw:
+  ; restore registers and return
+  PLA
+  TAY
+  PLA
+  TAX
+  PLA
+  PLP
+  RTS
+.endproc
+
+
 .segment "VECTORS"
 .addr nmi_handler, reset_handler, irq_handler
 
@@ -1454,7 +2067,7 @@ palettes:
   .byte $0f, $0f, $0f, $0f 
 
   .byte $0f ,$11, $21, $01
-  .byte $0f, $19, $09, $29
+  .byte $0f, $00, $10, $30
   .byte $0f, $19, $09, $29
   .byte $0f, $19, $09, $29
 
